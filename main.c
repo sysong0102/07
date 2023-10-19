e1c4b4f3a164767888fa1c3f70642fd47ed484f8
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-10-19 16:18:07 +0900

    Q6-2

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int all_files;
+static int all_files;
 
 extern void sub();
 
